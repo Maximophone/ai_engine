2bf7409f35e8a93eb379a93786d9c8188569f482
Maxime Fournes <fournes.maxime@gmail.com>
2025-04-25 14:33:53 +0200

    Update version number in setup.py to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ai_core',
-    version='0.1.0',
+    version='0.1.1',
     author='Your Name',
     author_email='your.email@example.com',
     description='A core library for interacting with various AI models.',
